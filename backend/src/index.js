5afe6ce1d51e45188d9bc03ed6d297e27e3dc3d4
anish11 <anishprasad2512@gmail.com>
2025-05-10 22:48:59 +0530

    fixed backed route issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -220,7 +220,7 @@ app.get("/logout", (req, res)=>{
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get("*", (req, res)=>{
+  app.get("/*", (req, res)=>{
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
   })
 }
