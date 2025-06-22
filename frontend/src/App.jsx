6daba39c3011811a163181c025e3cd608c73ee5e
anish11 <anishprasad2512@gmail.com>
2025-06-22 21:45:57 +0530

    test branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route, Navigate, useNavigate } from "react-router-dom"
 import Signup from "./pages/Signup"
 import ChatContainer from "./pages/ChatContainer"
 import { useEffect, useState } from "react"
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 import Test from "./components/Test"
 import { Userstore } from "./store/userStore"
 
@@ -12,43 +12,47 @@ import { Userstore } from "./store/userStore"
 function App() {
   const navigate = useNavigate()
 
-  const {validateUser, isloggedIn, currentsender, logoutHandler} = Userstore()
-
-  useEffect(()=>{
-    validateUser();
-  },[  validateUser])
-
-  useEffect(()=>{
-    console.log(`islogged is ${isloggedIn}`)
-  },[isloggedIn])
+  const { validateUser, isloggedIn, currentsender, logoutHandler } = Userstore()
 
   useEffect(() => {
-          // console.log(`current sender is ${JSON.stringify(currentsender)}`)
-          // console.log(`socket is ${JSON.stringify(socket)}`)
-          if(isloggedIn){
-              navigate("/home")
-          }else{
-            navigate("/login")
-          }
-        }, [currentsender, logoutHandler])
+    validateUser();
+  }, [validateUser])
+
+  useEffect(() => {
+    console.log(`islogged is ${isloggedIn}`)
+  }, [isloggedIn])
+
+  useEffect(() => {
+    // console.log(`current sender is ${JSON.stringify(currentsender)}`)
+    // console.log(`socket is ${JSON.stringify(socket)}`)
+    if (isloggedIn) {
+      navigate("/home")
+    } else {
+      navigate("/login")
+    }
+  }, [currentsender, logoutHandler])
 
   return (
     <div className="h-screen flex flex-col justify-center items-center w-screen bg-[#272b38c2]">
 
-   
-    <div className="bg-[#3B3E46] w-[90vw] h-[90vh] p-0 m-0 overflow-auto text-white flex flex-col justify-center items-center rounded-2xl">
-      <div className="" > 
-        <Navbar/>
+
+      <div className="bg-[#3B3E46] w-[90vw] h-[90vh] p-0 m-0 overflow-auto text-white flex flex-col justify-center items-center rounded-2xl">
+
+
+        {
+          isloggedIn ? <div className="" >
+            <Navbar />
+          </div> : null
+        }
+        <div className="  h-[75vh]">
+          <Routes>
+            <Route path="/" element={isloggedIn ? <ChatContainer /> : <Navigate to={"/login"} />} />
+            <Route path="/home" element={<ChatContainer />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
       </div>
-      <div className="  h-[75vh]">
-      <Routes>
-          <Route path="/" element={isloggedIn?<ChatContainer/>: <Navigate to={"/login"}/> }/>
-          <Route path="/home" element={<ChatContainer/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/signup" element={<Signup/>}/>
-      </Routes>
-      </div>
-    </div>
     </div>
   )
 }
